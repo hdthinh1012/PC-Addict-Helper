eff93e2a8232b0d0f171b7e107bcde6a5c5f3b6f
hdthinh1012 <hdthinh01@gmail.com>
2024-06-26 01:04:27 +0700

    thêm TIG stack, nginx route tới server go

--- one-liner.sh
+++ one-liner.sh
@@ -1,3 +1,6 @@
 docker-compose down
 # docker volume rm pc-addict-helper_postgres-data
+# docker volume rm pc-addict-helper_grafana-storage
+docker volume rm pc-addict-helper_influxdb2-config
+docker volume rm pc-addict-helper_influxdb2-data
 docker-compose up --build --remove-orphans
